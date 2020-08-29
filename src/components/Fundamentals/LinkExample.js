800a04066c730cbd2252e5b38b3c10407e67f93c
Victor Copque <victor@clarkeenergia.com>
2020-08-29 17:59:48 -0300

    WIP: insta feed

--- src/components/Fundamentals/LinkExample.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default function LinkExample() {
-  return <Link to="/fundamentals/use-state">Vai para outro lugar</Link>;
-};
